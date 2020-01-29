9a0982308738c71e7cdb96253f596f35462a2d43
Julio Lemos <juliodelemos@gmail.com>
2020-01-29 20:56:22 -0200

    Criando operacao de instanciacao

--- src/main/java/com/julio/cursomc/CursomcApplication.java
+++ src/main/java/com/julio/cursomc/CursomcApplication.java
@@ -1,13 +1,39 @@
 package com.julio.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import com.julio.cursomc.domain.Categoria;
+import com.julio.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner {
+
+	//cria uma dependencia com Depository, pois é esta camada que sabe acessar o BD
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	//Este método é da CommandLineRunner (vem apenas a assinatura,
+	//cabe ao desenvolvedor desenvolver o conteúdo do método
+	@Override
+	public void run(String... args) throws Exception {
+		//Criação de 2 objetos, conforme o Diagrama de Objetos
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//chama o método da categoriaRepository capaz de salvar os dados
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+			
+	}
+
+	
+	
 }
